ca0f106bf4624deaca671b6832fc5bbd61baae5d
Saif-041 <saif786999999@gmail.com>
2023-03-23 13:48:23 +0500

    Add chef & recipe model with validation. Db added.

---
+++ db/migrate/20230323072701_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.text :description
+      t.references :chef, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
