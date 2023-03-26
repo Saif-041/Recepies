03ee1451b595a0ead3aad8b15796fd72a3fd0dfa
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2023-03-26 21:39:47 +0500

    Add form for creating new recipe and show page.
    Add form for creating new recipe and show page.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,4 +12,20 @@ class RecipesController < ApplicationController
         @recipe = Recipe.new
     end
 
+    def create
+        @recipe = Recipe.new(recipes_params)
+        @recipe.chef = Chef.first
+        if @recipe.save
+            flash[:success] = "recipe created successfully"
+            redirect_to recipe_path(@recipe)
+        else
+            render 'new', status: 300
+        end
+    end
+
+    private
+        def recipes_params
+            params.require(:recipe).permit(:name, :description)
+        end
+
 end
