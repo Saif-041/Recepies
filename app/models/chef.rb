7b7d2fb08bb64fd884c4db49675c9c38b7c8ebf3
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2023-03-27 12:21:14 +0500

    chef work start
    work from home weekend update.

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,6 +3,8 @@ class Chef < ApplicationRecord
     validates :chefname, presence: true, length: { maximum: 30 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-    validates :chef_id, presence: true
+
+    validates :password, presence: true, length: { minimum: 5 }
     has_many :recipes
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   # get '/recipes', to: 'recipes#index'
   # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
   # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+
+  get '/signup', to: 'chefs#new'
+  resources :chefs, except: [:new]
   
 end
