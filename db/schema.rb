68741cafbd84aeb7ce96dad170b2dcacee9ab3ab
Saif-041 <saif786999999@gmail.com>
2023-03-27 13:05:26 +0500

    Created form for signup for chef.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_072701) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_26_203240) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "recipes", force: :cascade do |t|
