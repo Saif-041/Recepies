8db40e3b78e0a79576aa426db4ad34da2dba7e7d
Saif-041 <saif786999999@gmail.com>
2023-03-28 12:36:07 +0500

    Comments added. Styling added. Refactor. Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root "pages#home"
   get "pages/home", to: 'pages#home'
 
-  resources :recipes
+  resources :recipes do
+    resources :comments, only: [:create]
+  end
 
   # get '/recipes', to: 'recipes#index'
   # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
